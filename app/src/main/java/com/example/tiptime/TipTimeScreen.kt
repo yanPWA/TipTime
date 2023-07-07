1b802660545ad233d06a707edcd9cf5abbcf7417
藤田彰子 <akiko@yanMacBook-Air.local>
2023-07-07 16:07:10 +0900

    [add]金額計算の処理を実装し、それに合わせて状態保持する場所をEditNumberFieldからTipTimeScreenに変更

--- app/src/main/java/com/example/tiptime/TipTimeScreen.kt
+++ app/src/main/java/com/example/tiptime/TipTimeScreen.kt
@@ -27,49 +27,61 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tiptime.ui.theme.TipTimeTheme
+import java.text.NumberFormat
 
 @Composable
-fun TipTimeScreen() {
+fun TipTimeScreen(calculateTip: (Double) -> String) {
     Column(
         modifier = Modifier.padding(32.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
+        var amountInput by remember { mutableStateOf("") }
+
+        // StringからDoubleに型変換
+        val amount = amountInput.toDoubleOrNull() ?: 0.0
+        // 計算実行した結果を変数に格納
+        val tip = calculateTip.invoke(amount)
+
         Text(
             text = stringResource(id = R.string.calculate_tip),
             fontSize = 24.sp,
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Spacer(Modifier.height(16.dp))
-        EditNumberField()
-
+        EditNumberField(
+            value = amountInput,
+            onValueChange = { amountInput = it }
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = stringResource(id = R.string.tip_amount, tip),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EditNumberField() {
-    var amountInput by remember { mutableStateOf("") }
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
     TextField(
-        value = amountInput,
-        onValueChange = { amountInput = it },
+        value = value,
+        onValueChange = onValueChange,
         label = { Text(stringResource(R.string.cost_of_service)) },
         modifier = Modifier.fillMaxWidth(),
         singleLine = true,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
     )
-    Spacer(modifier = Modifier.height(24.dp))
-    Text(
-        text = stringResource(id = R.string.tip_amount, ""),
-        fontSize = 20.sp,
-        fontWeight = FontWeight.Bold
-        )
 }
-
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TipTimeTheme {
-        TipTimeScreen()
+        TipTimeScreen {_ -> "$15.00"}
     }
 }
