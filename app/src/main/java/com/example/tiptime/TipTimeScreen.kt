370bd7259af4529df1aa7fda0413003652d9a3be
藤田彰子 <akiko@yanMacBook-Air.local>
2023-07-10 09:40:31 +0900

    [add]チップパーセンテージ入力欄を追加し各キーボードアクションを設定

--- app/src/main/java/com/example/tiptime/TipTimeScreen.kt
+++ app/src/main/java/com/example/tiptime/TipTimeScreen.kt
@@ -1,12 +1,14 @@
 package com.example.tiptime
 
-import android.annotation.SuppressLint
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -14,34 +16,39 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tiptime.ui.theme.TipTimeTheme
-import java.text.NumberFormat
 
 @Composable
-fun TipTimeScreen(calculateTip: (Double) -> String) {
+fun TipTimeScreen(calculateTip: (Double, Double) -> String) {
+    var amountInput by remember { mutableStateOf("") }
+    var tipInput by remember { mutableStateOf("") }
+
+    // StringからDoubleに型変換
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+    // 計算実行した結果を変数に格納
+    val tip = calculateTip.invoke(amount, tipPercent)
+
+    val focusManager = LocalFocusManager.current
+
     Column(
         modifier = Modifier.padding(32.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        var amountInput by remember { mutableStateOf("") }
-
-        // StringからDoubleに型変換
-        val amount = amountInput.toDoubleOrNull() ?: 0.0
-        // 計算実行した結果を変数に格納
-        val tip = calculateTip.invoke(amount)
-
         Text(
             text = stringResource(id = R.string.calculate_tip),
             fontSize = 24.sp,
@@ -49,8 +56,24 @@ fun TipTimeScreen(calculateTip: (Double) -> String) {
         )
         Spacer(Modifier.height(16.dp))
         EditNumberField(
+            label = R.string.bill_amount,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onNext = { focusManager.moveFocus(FocusDirection.Down) }),
             value = amountInput,
             onValueChange = { amountInput = it }
+        )
+        EditNumberField(
+            label = R.string.how_was_the_service,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+            value = tipInput,
+            onValueChange = { tipInput = it }
         )
         Spacer(modifier = Modifier.height(24.dp))
         Text(
@@ -62,19 +85,27 @@ fun TipTimeScreen(calculateTip: (Double) -> String) {
     }
 }
 
+/**
+ * チップの計算
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditNumberField(
+    @StringRes label: Int,
+    keyboardOptions: KeyboardOptions,
+    keyboardActions: KeyboardActions,
     value: String,
-    onValueChange: (String) -> Unit
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
 ) {
     TextField(
         value = value,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
         onValueChange = onValueChange,
-        label = { Text(stringResource(R.string.cost_of_service)) },
+        label = { Text(stringResource(label)) },
         modifier = Modifier.fillMaxWidth(),
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
     )
 }
 
@@ -82,6 +113,6 @@ fun EditNumberField(
 @Composable
 fun DefaultPreview() {
     TipTimeTheme {
-        TipTimeScreen {_ -> "$15.00"}
+        TipTimeScreen { _, _ -> "$15.00" }
     }
 }
